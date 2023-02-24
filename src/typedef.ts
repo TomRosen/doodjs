3bcb51eba2b78bf6595c29adec75900e5f26899d
Tom Rosen <55321560+TomRosen@users.noreply.github.com>
2023-02-24 23:40:36 +0100

    Fixed typedef for recent commit

--- src/typedef.ts
+++ src/typedef.ts
@@ -1,18 +1,18 @@
 export interface Directive {
-	name: string;
-	fn: (ctx: DirectiveContext, target: DoodData) => void;
+  name: string;
+  fn: (ctx: DirectiveContext, target: DoodData) => void;
 }
 
 export interface DoodData {
-	[key: string]: any;
+  [key: string]: any;
 }
 
 export interface DirectiveContext {
-	el: Element;
-	expr: string;
-	arg?: string;
-	modifiers?: string[];
-	run: (expression?: string) => any;
+  el: Element;
+  expr: string;
+  arg?: string;
+  modifiers?: string[];
+  run: (expression?: string, args?: Array<any>) => any;
 }
 
 export type effect = () => void;
